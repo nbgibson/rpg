5d0b57d889004aeb25c2b88dd0aed43ff3f98501
N Gibson <nbgibson@users.noreply.github.com>
2020-05-10 23:51:42 -0400

    Lesson 25 complete

--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -6,32 +6,17 @@ using UnityEngine.AI;
 public class Mover : MonoBehaviour
 {
     [SerializeField] Transform target;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButton(0)){
-            MoveToCursor();
-        }
         UpdateAnimator();
-        
     }
 
-    private void MoveToCursor()
+    
+    public void MoveTo(Vector3 destination)
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        bool hasHit = Physics.Raycast(ray, out hit);
-        if (hasHit)
-        {
-            GetComponent<NavMeshAgent>().destination = hit.point;
-        }
+      GetComponent<NavMeshAgent>().destination = destination;
     }
 
     private void UpdateAnimator(){
@@ -39,5 +24,5 @@ public class Mover : MonoBehaviour
         Vector3 localVelocity = transform.InverseTransformDirection(velocity);
         float speed = localVelocity.z;
         GetComponent<Animator>().SetFloat("forwardSpeed", speed);
-    }
+      }
 }
