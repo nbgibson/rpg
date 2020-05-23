0a1923825d5b86a17312891d0b1b828ce8e3df22
N Gibson <nbgibson@users.noreply.github.com>
2020-05-23 14:50:57 -0400

    Lesson 29 completed

--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -8,7 +8,12 @@ namespace RPG.Movement
     public class Mover : MonoBehaviour
     {
         [SerializeField] Transform target;
+        NavMeshAgent navMeshAgent;
 
+        private void Start(){
+            navMeshAgent = GetComponent<NavMeshAgent>();
+        }
+        
         // Update is called once per frame
         void Update()
         {
@@ -18,11 +23,16 @@ namespace RPG.Movement
 
         public void MoveTo(Vector3 destination)
         {
-            GetComponent<NavMeshAgent>().destination = destination;
+            navMeshAgent.destination = destination;
+            navMeshAgent.isStopped = false;
+        }
+
+        public void Stop(){
+            navMeshAgent.isStopped = true;
         }
 
         private void UpdateAnimator(){
-            Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+            Vector3 velocity = navMeshAgent.velocity;
             Vector3 localVelocity = transform.InverseTransformDirection(velocity);
             float speed = localVelocity.z;
             GetComponent<Animator>().SetFloat("forwardSpeed", speed);
