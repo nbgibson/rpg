cc4069e8846301e7253c5072daaf542ad00f6353
N Gibson <nbgibson@users.noreply.github.com>
2020-08-01 00:09:25 -0400

    Lesson 41 completed

--- Assets/Scripts/Combat/CombatTarget.cs
+++ Assets/Scripts/Combat/CombatTarget.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 namespace RPG.Combat
 {
+
+    [RequireComponent(typeof(Health))]
     public class CombatTarget : MonoBehaviour {
-        
     }
 }
--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -2,9 +2,14 @@
 
 namespace RPG.Combat{
     public class Health : MonoBehaviour {
+
         [SerializeField] float healthPoints = 100f;
 
-        bool isDead = false;
+        [SerializeField] bool isDead = false;
+
+        public bool IsDead(){
+            return isDead;
+        }
 
         public void TakeDamage(float damage){
             healthPoints = Mathf.Max(healthPoints - damage, 0);
